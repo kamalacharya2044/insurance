947517f40734d53f7c6874b2f5344400a948d484
Kamal Acharya <lotusachaya@gmail.com>
2019-07-13 12:05:16 +0545

    added pytest and sample testing

---
+++ dummy.py
@@ -0,0 +1,12 @@
+def is_prime(val):
+    """Improve this function with test"""
+    if val < 2:
+        return False
+    elif val == 2:
+        return True
+    else:
+        return False
+
+
+def sumof(val):
+    return val + val
---
+++ test_app.py
@@ -0,0 +1,16 @@
+from dummy import is_prime
+from dummy import sumof
+
+
+def test_is_prime():
+    assert not is_prime(1)
+    assert is_prime(2)
+    assert is_prime(3)
+
+
+def test_sumof():
+    assert sumof(1) == 2
+    assert sumof(2) == 4
+    assert sumof(3) == 6
+    assert sumof(4) == 8
+    assert sumof(5) == 10
